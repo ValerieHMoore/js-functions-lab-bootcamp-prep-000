99bf064e9dbb1e190c446c0569dcf65fe2040159
Valerie Moore <periwinkle17@outlook.com>
2018-04-09 19:59:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,4 @@ function happyHolidaysTo(name) {
 return 'Happy holidays, you!'
 }
 function happyHolidayTo(holiday,name) {
-return ('Happy ${holiday}, ${name}!')
-happyHolidayTo(Easter,Valerie)
-}
+return 
